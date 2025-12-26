b5c0a2cadb92e12860cc66f9e90e604cb8771381
Human <aptech.daniyal0@gmail.com>
2025-12-26 17:05:11 +0500

    Nav Fix

--- Semester-Demo Project/main.js
+++ Semester-Demo Project/main.js
@@ -1,6 +1,16 @@
-const logoBtn = document.getElementById('logoBtn');
-const megaMenu = document.getElementById('megaMenu');
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
 
-logoBtn.addEventListener('click', function () {
-    megaMenu.classList.toggle('active');
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+
+// Close menu when clicking on a link
+const navItems = document.querySelectorAll('.nav-links li a');
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navLinks.classList.remove('active');
+    });
 });
