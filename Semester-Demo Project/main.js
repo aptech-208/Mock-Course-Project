62557042834b90617ee3fc91f45dcd489d677045
Human <aptech.daniyal0@gmail.com>
2026-01-02 17:02:04 +0500

    Contact Page Linked

--- Semester-Demo Project/main.js
+++ Semester-Demo Project/main.js
@@ -1,24 +1,25 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('navLinks');
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
 
-hamburger.addEventListener('click', function () {
-    hamburger.classList.toggle('active');
-    navLinks.classList.toggle('active');
+hamburger.addEventListener("click", function () {
+    // hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
 });
 
-const navItems = document.querySelectorAll('.nav-links li a');
+const navItems = document.querySelectorAll(".nav-links li a");
 navItems.forEach(item => {
-    item.addEventListener('click', () => {
-        hamburger.classList.remove('active');
-        navLinks.classList.remove('active');
+    item.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        // navLinks.classList.remove("active");
     });
 });
 
-window.addEventListener('scroll', function () {
-    const nav = document.querySelector('nav');
-    if (window.scrollY > 300) {
-        nav.classList.add('sticky-active');
+// Smooth scrolling for nav bar
+window.addEventListener("scroll", function () {
+    const nav = document.querySelector("nav");
+    if (window.scrollY > 400) {
+        nav.classList.add("sticky-active");
     } else {
-        nav.classList.remove('sticky-active');
+        nav.classList.remove("sticky-active");
     }
 });
